6d1fbcc767d93cadaec93daf6154c84df54dc4af
braIntelligent <m.cataldoolate@gmail.com>
2025-05-10 19:42:03 -0400

    fix-server-port

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const path = require('path');
 
 const app = express();
 app.use(cors());
